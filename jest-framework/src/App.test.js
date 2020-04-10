28700507065f6539036cb34b4e12d93c77ab0d8b
Fabian Andres Levin <fabianandreslevin@Fabians-MacBook-Pro.local>
2020-04-09 22:48:01 -0300

    basic jest framework

--- jest-framework/src/App.test.js
+++ jest-framework/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+describe('test suite', () => {
+  test('renders learn react link', () => {
+    const wrapper = shallow(<App />);
+    
+    expect(wrapper).toBeTruthy();
+  });
+})
+
