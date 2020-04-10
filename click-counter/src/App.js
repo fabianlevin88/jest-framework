2a32d03ec918c11570b311f9a90690849555103a
Fabian Andres Levin <fabianandreslevin@Fabians-MacBook-Pro.local>
2020-04-10 01:28:56 -0300

    Click counter project and test finished

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
+// eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
   
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  }
+
   render() {
     return(
       <div className="ui container">
-        <div data-test="component-app">
-          <h1>App</h1>
+        <div className="ui segment">
+          <div data-test="component-app">
+            <h1 className="ui header" data-test="title">Counter App</h1>
+          </div>
+          <div>
+            <h2 className="ui sub header" data-test="increment-display">The counter is currently: <span className="ui large header">{this.state.counter}</span></h2>
+            <button 
+              className="ui primary button" 
+              data-test="increment-button"
+              onClick={() => this.setState({counter: this.state.counter + 1})}
+            >
+            Increment
+            </button>
+          </div>
         </div>
       </div>
     )
   }
-
 }
 
 export default App;
--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -6,30 +6,94 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
+/**
+ * Factory function to create a shallow wrapperfor the App component
+ * @function setup
+ * @param {object} props - Component props specific for this setup
+ * @param {object} state - Initial state for setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props={}, state=null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper
+}
+
+/**
+ * Returns a ShallowWrapper containing node(s) with the givenn data-test value
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+ * @param {string} val - Value of the data-test attribute for search
+ * @returns {ShallowWrapper}
+ */
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`)
+}
+
 describe('Counter App Test Suite', () => {
 
-  const wrapper = shallow(<App />);
+  test('Validate that the App title is correctly displayed', () => {
+
+    const wrapper = setup();
+
+    const titleComponent = findByTestAttr(wrapper, 'title');
+
+    expect(titleComponent.text()).toBe('Counter App');
+
+    expect(titleComponent.html()).toBe('<h1 class="ui header" data-test="title">Counter App</h1>');
+  });
 
   test('Validate that the app renders without any error', () => {
 
-    const appComponent = wrapper.find("[data-test='component-app']")
+    const wrapper = setup();
+
+    const appComponent = findByTestAttr(wrapper, 'component-app');
 
     expect(appComponent.length).toBe(1);
   });
 
   test('Validate that the increment button is rendered', () => {
 
+    const wrapper = setup();
+    
+    const incrementButton = findByTestAttr(wrapper, 'increment-button');
+
+    expect(incrementButton.length).toBe(1);
   });
 
-  test('Validate that the counter label is rendered', () => {
+  test('Validate that the counter display is rendered', () => {
 
+    const wrapper = setup();
+    
+    const incrementLabel = findByTestAttr(wrapper, 'increment-display');
+
+    expect(incrementLabel.length).toBe(1);
   });
 
   test('Validate that the counter starts at 0', () => {
+    
+    const wrapper = setup();
+    
+    const initialCounterState = wrapper.state('counter')
 
+    expect(initialCounterState).toBe(0)
   });
 
   test('Validate that clicking increments the counter displayed', () => {
 
+    // Setting initial state
+    const counter = 7;
+    const wrapper = setup(null, {counter});
+    
+    // Finding the increment button
+    const incrementButton = findByTestAttr(wrapper, 'increment-button');
+
+    // Simulating the button's click
+    incrementButton.simulate('click');
+
+    // Finding the increment label
+    const incrementLabel = findByTestAttr(wrapper, 'increment-display');
+
+    
+    expect(incrementLabel.text()).toContain(counter + 1);
   });
 })
